4bdc38ef469bc224a71bcca8517fef7fd9152389
Катя Смолякова <blackhepburn@gmail.com>
2018-03-07 00:18:54 +0300

    1st commit

--- BarCodeSvannerDemo/ViewController.swift
+++ BarCodeSvannerDemo/ViewController.swift
@@ -7,14 +7,23 @@
 //
 
 import UIKit
+import BarcodeScanner
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func scanAction(_ sender: Any) {
+        
+        let viewController = BarcodeScannerViewController()
+        viewController.codeDelegate = self
+        
+        present(viewController, animated: true, completion: nil)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
