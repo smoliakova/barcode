4dc076c2886bbf9ca119b912a6780bfd201532aa
Катя Смолякова <blackhepburn@gmail.com>
2018-03-18 14:50:40 +0300

    add scanAction code

--- BarCodeSvannerDemo/ViewController.swift
+++ BarCodeSvannerDemo/ViewController.swift
@@ -10,13 +10,14 @@ import UIKit
 import BarcodeScanner
 
 class ViewController: UIViewController {
+    
     @IBOutlet weak var webView: UIWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     @IBAction func scanAction(_ sender: Any) {
         
         let viewController = BarcodeScannerViewController()
@@ -24,6 +25,25 @@ class ViewController: UIViewController {
         
         present(viewController, animated: true, completion: nil)
     }
+}
+    
+extension ViewController : BarcodeScannerCodeDelegate {
+            func scanner(_ controller: BarcodeScannerViewController, didCaptureCode code: String, type: String) {
+               guard let url = URL(string: "http://ean13.info/\(code).htm") else
+               {return}
+                webView.loadRequest(URLRequest(url: url))
+                
+            }
+        }
+
+    
+    
+    
+    
+    
+    
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
